2fe90d448dea4a1061d655b2ca504accf531bd41
dmistry <84239029+Dm1stry@users.noreply.github.com>
2023-08-16 02:23:19 +0300

    Update install-qt5.sh
    Fixed problem with missing Qt5LinguistTools

--- install-qt5.sh
+++ install-qt5.sh
@@ -2,7 +2,7 @@ echo "Qt5 installation script\nRun with sudo\n"
 
 sudo apt update
 sudo apt install -y build-essential
-sudo apt install -y qtcreator qtbase5-dev qt5-qmake cmake
+sudo apt install -y qtcreator qtbase5-dev qt5-qmake qttools5-dev cmake
 sudo apt install -y qt5-doc qt5-doc-html qtbase5-doc-html
 sudo apt install -y qtbase5-examples
 
